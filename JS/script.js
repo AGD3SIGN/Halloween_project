12b00f2aff36bb957bf752e4a28e1a5df4474b98
GoatEXE <DTCoppock@gmail.com>
2024-10-18 09:41:32 -0500

    Functionality Update
    1. Added favicon and changed title
    
    2. Added script to have a "play button", which should enable unmute/autoplay after having interacted with page.

---
+++ JS/script.js
@@ -0,0 +1,26 @@
+hmm = true
+
+document.addEventListener('click', function() {
+    // Run only once per document load
+    if (hmm === true) {
+
+    // Targets
+    var cta = document.getElementById('cta');
+    var title = document.getElementById('title');
+    var iframe = document.getElementById('youtube-video');
+    var card = document.getElementById('card');
+    var bottomHeading = document.getElementById('bottom-heading');
+
+    // Autoplay video and unmute
+    iframe.src = iframe.src + "?autoplay=1&mute=0";
+
+    // Show/hide functionality
+    cta.style.display = 'none';
+    title.style.display = 'inherit';
+    card.style.display = 'flex';
+    bottomHeading.style.display = 'inherit';
+    }
+
+    // Toggle
+    hmm = false;
+});
